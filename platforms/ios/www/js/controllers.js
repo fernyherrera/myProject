d5366f3dc20095c67981a8bb632bb4945c36602b
Fernando <fherrera85@gmail.com>
2014-07-08 17:26:50 -0700

    second commit

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,6 +1,19 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+  $scope.cards = [
+    {title:"Marty McFly",date:"November 05, 1955", image: "img/mcfly.jpg", body:"This is a \"Facebook\" styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer.", likes:1, comments:5},
+    {title:"Marty McFly",date:"November 05, 1955", image: "img/mcfly.jpg", body:"This is a \"Facebook\" styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer.", likes:1, comments:5}
+  ];
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.cardSwiped = function(index) {
+    var newCard = // new card data
+      $scope.cards.push(newCard);
+  };
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,26 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+  var cardTypes = [
+    {title:"Marty McFly",date:"November 05, 1955", image: "img/mcfly.jpg", body:"This is a \"Facebook\" styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer.", likes:1, comments:5},
+    {title:"Marty McFly",date:"November 05, 1955", image: "img/mcfly.jpg", body:"This is a \"Facebook\" styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer.", likes:1, comments:5}
+  ];
+
+  $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
+
+  $scope.cardSwiped = function(index) {
+    $scope.addCard();
+  };
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+  }
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
